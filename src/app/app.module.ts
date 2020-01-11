46ea683b0a4c8eeea937782d4c9085b215666ff0
Manoth Klangphila <manothklangphila@Manoths-MacBook-Pro.local>
2020-01-11 14:55:31 +0700

    test3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
@@ -20,7 +22,8 @@ import { LoginComponent } from './pages/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public login = new Login();
+
   constructor() {
     document.body.className = 'hold-transition login-page';
   }
@@ -14,4 +16,16 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSubmit() {
+    if (this.login.username == 'admin' && this.login.password == 'admin') {
+      alert('Login Success!');
+    } else {
+      alert('Login Fail !');
+    }
+  }
+
+}
+export class Login {
+  username: string;
+  password: string;
 }
