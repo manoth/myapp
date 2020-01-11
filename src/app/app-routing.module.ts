bc562c8b0733584494838382eafed71383ee08ec
Manoth Klangphila <manothklangphila@Manoths-MacBook-Pro.local>
2020-01-11 15:55:47 +0700

    test9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { ConentComponent } from './contents/conent/conent.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'index', component: ConentComponent },
+  {
+    canActivate: [AuthGuardService],
+    children: [
+      { path: 'index', component: ConentComponent }
+    ]
+  },
   { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
 
